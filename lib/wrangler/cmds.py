47195084eb6711d23b068acd7135a7dc21fc4574
adorsk <adorsk>
2013-01-14 09:35:00 -0500

    added up date action, targets

--- lib/wrangler/cmds.py
+++ lib/wrangler/cmds.py
@@ -10,6 +10,10 @@ argparser = argparse.ArgumentParser(
     description='Manage assets.'
 )
 argparser.add_argument('action', help='Action to execute')
+argparser.add_argument('targets', help=(
+    'Specific assets to act. If left empty', 'acts on all assets'),
+    nargs='*')
+
 argparser.add_argument('-f', '--assetfile', type=str, default='AssetFile.py', 
                        help='AssetFile to use')
 argparser.add_argument('-c', type=str, default='.wrangler.py', 
@@ -17,53 +21,80 @@ argparser.add_argument('-c', type=str, default='.wrangler.py',
 argparser.add_argument('-t', '--targetdir', type=str, 
                        help='Target dir to put assets into.')
 
+def initialize_asset_file(asset_file):
+    if os.path.exists(asset_file):
+        print >> sys.stderr, "Already initialized."
+        return
+
+    asset_file_tpl = os.path.join(os.path.dirname(os.path.abspath(__file__)), 
+                                  'templates', 'AssetFile.py')
+    shutil.copy(asset_file_tpl, asset_file)
+    print >> sys.stderr, "Initialized with '%s'." % asset_file
+
 class Wrangler(object):
-    def __init__(self, asset_file=None, config={}):
-        self.asset_file = asset_file
-        assets_data = self.read_asset_file()
+    def __init__(self, asset_file=None, config={}, targets=None):
+        self.assets_data = self.read_asset_file(asset_file)
 
         # Update config w/ options from command line.
         self.config = {}
-        self.config.update(assets_data.get('config', {}))
+        self.config.update(self.assets_data.get('config', {}))
         self.config.update(config)
 
-    def init(self):
-        """ Create the initial AssetFile. """
-        if os.path.exists(self.asset_file):
-            print >> sys.stderr, "Already initialized."
-            return
+        self.setup_dirs()
+        self.target_defs = self.get_target_defs(targets)
 
-        asset_file_tpl = os.path.join(os.path.dirname(os.path.abspath(__file__)), 
-                                      'templates', 'AssetFile.py')
-        shutil.copy(asset_file_tpl, self.asset_file)
-        print >> sys.stderr, "Initialized with '%s'." % self.asset_file
-
-    def clean(self):
-        """ Clear out the target dir. """
-        assets_data = self.read_asset_file()
-        for item in os.listdir(self.config['TARGET_DIR']):
-            item_path = os.path.join(self.config['TARGET_DIR'], item)
-            print >> sys.stderr, "removing '%s'..." % item_path
-            shutil.rmtree(item_path)
-
-    def install(self):
-        """ Install assets in target dir. """
-        assets_data = self.read_asset_file()
+    def setup_dirs(self):
         for dir_ in [self.config['CACHE_DIR'], self.config['TARGET_DIR']]:
             if not os.path.exists(dir_):
                 os.makedirs(dir_)
 
+    def get_target_defs(self, targets):
+        if targets:
+            return dict(
+                [(k, v) for k, v in self.assets_data['assets'].iteritems() 
+                 if k in targets])
+        else:
+            return self.assets_data['assets']
+
+    def clean(self):
+        """ Clear out assets in the target dir. """
+        for asset_id in self.target_defs.keys():
+            self.remove_asset(asset_id)
+
+    def remove_asset(self, asset_id):
+        target_path = os.path.join(self.config['TARGET_DIR'], asset_id)
+        print >> sys.stderr, "removing '%s'..." % target_path
+        if os.path.exists(target_path):
+            if os.path.isdir(target_path):
+                shutil.rmtree(target_path)
+            else:
+                os.remove(target_path)
+
+    def install(self):
+        """ Install assets in target dir. """
         processor = Processor(
             cache_dir=self.config['CACHE_DIR'],
             target_dir=self.config['TARGET_DIR']
         )
-        processor.resolve_asset_defs(assets_data['assets'])
+        processor.resolve_asset_defs(self.target_defs)
         print >> sys.stderr, "Install finished."
 
-    def read_asset_file(self):
+    def update(self):
+        """ Update assets. Removes and then re-installs assets. """
+        processor = Processor(
+            cache_dir=self.config['CACHE_DIR'],
+            target_dir=self.config['TARGET_DIR']
+        )
+        print self.target_defs
+        for asset_id, asset_def in self.target_defs.items():
+            self.remove_asset(asset_id)
+            processor.resolve_asset_defs({asset_id: asset_def})
+        print >> sys.stderr, "Update finished."
+
+    def read_asset_file(self, asset_file):
         """ Read data from asset file. """
         assets_data = {}
-        execfile(self.asset_file, assets_data)
+        execfile(asset_file, assets_data)
         return assets_data
 
 if __name__ == '__main__':
@@ -78,7 +109,10 @@ if __name__ == '__main__':
         value = getattr(args, arg, None)
         if value is not None:
             config[config_attr] = value
-
-    wrangler = Wrangler(asset_file=args.assetfile, config=config)
-    action_method = getattr(wrangler, action)
-    action_method()
+    
+    if action == 'init':
+        initialize_asset_file(args.assetfile)
+    else:
+        wrangler = Wrangler(asset_file=args.assetfile, config=config,
+                        targets=args.targets)
+        getattr(wrangler, action)()
